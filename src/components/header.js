5ee3f79cedfe1d401d612de4056ab89f4c6f8f12
Andy Tran <trandromeda@Fantasea.local>
2019-02-01 18:06:51 -0500

    created header, button and lesson-box

---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="header">
+                This is where the header will be.
+            </div>
+        )
+    }
+}
+
+export default Header;
