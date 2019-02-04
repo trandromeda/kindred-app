134f564c3465eee7c83388f9ebb3329b8e566b39
Andy Tran <transient.tran@gmail.com>
2019-02-04 10:19:52 -0500

    added a few things to the lesson page

--- src/views/lesson.js
+++ src/views/lesson.js
@@ -4,14 +4,41 @@ import { BrowserRouter as Router, Route, Link, withRouter } from "react-router-d
 
 import { Header, LessonBox } from '../components';
 
+const Input = styled.input`
+  padding: 0.5em;
+  margin: 0.5em;
+  color: palevioletred;
+  background: papayawhip;
+  border: none;
+  border-radius: 3px;
+`;
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Column = styled.div`
+  padding: 25px;
+`;
+
 class Lesson extends React.Component {
   render() {
     console.log(this.props.match);
     return (
-      <div>
+      <Container>
+        <Column>
           <h1>Lesson {this.props.match.params.lessonId}</h1>
           <p>Something</p>
-      </div>
+        </Column>
+        <Column>
+          <h1>Details</h1>
+          <form>
+            <Input type="text" />
+          </form>
+        </Column>
+      </Container>
     );
   }
 }
