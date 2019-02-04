0e3b56b065bf5ece93056f028e12d0dae78d4795
Andy Tran <transient.tran@gmail.com>
2019-02-04 13:09:30 -0500

    laid out options in the search

--- src/components/search.js
+++ src/components/search.js
@@ -4,16 +4,46 @@ import styled from 'styled-components';
 const Search = styled.div`
     width: 100%;
     height: 150px;
-    background: pink;
+    background: #F2DC5D;
+    border-bottom: 2px solid #32DBC4;
 `
+
+const Heading = styled.h1`
+    margin: 0;
+    padding: 10px 0;
+    text-align: center;
+`;
+
+const Options = styled.div`
+    display: flex;
+    justify-content: space-around;
+`;
+
+const Button = styled.div`
+    width: 250px;
+    height: 55px;
+    padding: 10px 15px;
+    background: #32DBC4;
+    
+    p {
+        margin: 0;
+    }
+`;
 
 class Searchbar extends Component {
     render() {
         return (
             <Search>
-                <form>
-                    <input type="text"></input>
-                </form>
+                <Heading>What will it be?</Heading>
+                <Options>
+                    <Button><p>I want to be a better cook</p></Button>
+                    <Button><p>I want to better manage my finances</p></Button>
+                    <Button><p>I want to...</p>
+                    <form>
+                        <input type="text"></input>
+                    </form>
+                        </Button>
+                </Options>
             </Search>
         )
     }
