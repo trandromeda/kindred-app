3740f4d6e61ddf9e318e1e72a7e4b56eefaa8656
Andy Tran <transient.tran@gmail.com>
2019-02-04 17:08:30 -0500

    created a dynamic guided walkthrough with the lesson planner

--- src/views/mentor.js
+++ src/views/mentor.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
+const Container = styled.div`
+  padding: 15px 25px;
+  display: flex;
+
+  div:first-child {
+    flex: 0 1 600px;
+  }
+`;
+
 const Form = styled.form`
   display: flex;
   flex-direction: column;
@@ -18,11 +27,49 @@ class Mentor extends Component {
       desc: '',
       location: 'Home',
       title: '',
+      guide: '',
     };
   }
 
   handleChange = event => {
     this.setState({ [event.target.name]: event.target.value })
+  }
+
+  handleFocus = event => {
+    const { name } = event.target;
+    console.log(name);
+
+    switch (name) {
+      case 'category': {
+        this.setState({
+          guide: 'What are the core skills you are teaching?'
+        })
+        break;
+      }
+      case 'desc': {
+        this.setState({
+          guide: 'Talk about why you want to teach this lesson and how you learned it yourself.'
+        })
+        break;
+      }
+      case 'location': {
+        this.setState({
+          guide: 'Please choose from the list of pre-approved locations where you will host this lesson.'
+        })
+        break;
+      }
+      case 'title': {
+        this.setState({
+          guide: 'Choose something catching and brief!'
+        })
+        break;
+      }
+      default: {
+        this.setState({
+          guide: 'Welcome!'
+        })
+      }
+    }
   }
 
   handleSubmit = event => {
@@ -31,13 +78,16 @@ class Mentor extends Component {
   }
 
   render() {
+    const { guide } = this.state;
+    console.log(guide);
     return (
-      <div className="container">
+      <Container>
+        <div>
           <h1>Plan Your Lesson</h1>
           <Form onSubmit={this.handleSubmit}>
             <label>
               What kind of lesson are you planning?
-            <select value={this.state.category} name="category" onChange={this.handleChange} >
+            <select value={this.state.category} name="category" onChange={this.handleChange} onFocus={this.handleFocus}>
               <option value="cooking">Cooking</option>
               <option value="home-economics">Home Economics</option>
               <option value="arts">Arts and Crafts</option>
@@ -47,7 +97,7 @@ class Mentor extends Component {
 
             <label>
               Please describe the lesson:
-              <textarea value={this.state.desc} name="desc" onChange={this.handleChange} />
+              <textarea value={this.state.desc} name="desc" onChange={this.handleChange} onFocus={this.handleFocus} />
             </label>
 
 
@@ -58,7 +108,7 @@ class Mentor extends Component {
 
             <label>
               Where will the lesson be held?
-                <select value={this.state.location} name="location" onChange={this.handleChange} >
+                <select value={this.state.location} name="location" onChange={this.handleChange} onFocus={this.handleFocus}>
                   <option value="home">My home</option>
                   <option value="centre">Community center</option>
                   <option value="school">Local school</option>
@@ -67,12 +117,17 @@ class Mentor extends Component {
 
             <label>
               Lastly, give your lesson a name!
-              <input value={this.state.title} name="title" onChange={this.handleChange} />
+              <input value={this.state.title} name="title" onChange={this.handleChange} onFocus={this.handleFocus} />
             </label>
 
             <input type="submit" text="Submit" />
           </Form>
-      </div>
+        </div>
+        <div>
+          <h2>Guided walkthrough</h2>
+          {guide}
+        </div>
+    </Container>
     );
   }
 }
