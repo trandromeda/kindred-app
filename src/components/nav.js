31fc8ff981bf009af52b0fbaa10c3bea252f844a
Andy Tran <transient.tran@gmail.com>
2019-02-04 13:10:19 -0500

    make the logo link back to the home page

--- src/components/nav.js
+++ src/components/nav.js
@@ -33,7 +33,7 @@ class Nav extends Component {
     render() {
         return (
             <Navbar>
-                <img src={logo} />
+                <Link to="/"><img src={logo} /></Link>
                 <NavList>
                     <NavItem>
                         <Link to="/mentor">Mentor</Link>
