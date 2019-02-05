4a9a1b2ce9313fde5ad8b88690c1f70db3ec4523
Andy Tran <transient.tran@gmail.com>
2019-02-05 00:26:21 -0500

    Populate dashboard with more stuff

--- src/views/dashboard.js
+++ src/views/dashboard.js
@@ -1,17 +1,41 @@
 import React, { Component } from 'react';
-// import styled from 'styled-components';
+import styled from 'styled-components';
 import { connect } from 'react-redux';
+
+const Container = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+
+    h1 {
+        flex: 0 1 100%;
+        text-align: center;
+    }
+
+    div {
+        width: 45%;
+        padding: 10px 25px;
+    }
+`;
 
 class Dashboard extends Component {
   render() {
     const { requests } = this.props;
     return (
-      <div>
+      <Container>
           <h1>Welcome to your Dashboard</h1>
-          {requests && requests.length && requests.map((request, index) => {
-                return <p key={index}>{request}</p>
-            })}
-      </div>
+          <div>
+            <h3>You have upcoming lessons:</h3>
+            <h3>Your past lessons:</h3>
+            <p>Your account is being shared with: Molly Simmons</p>
+          </div>
+          <div>
+            <h3>These skills are in demand:</h3>
+            {requests && requests.length && requests.map((request, index) => {
+                    return <p key={index}>{request}</p>
+                })}
+          </div>
+      </Container>
     );
   }
 }
